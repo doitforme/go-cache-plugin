6cc15593004d69c49e4e03ec333737832c14b5bc
Kotov Roman Olegovich <roman_kotov@ozon.ru>
2025-05-05 15:32:10 +0300

    module

--- cmd/go-cache-plugin/setup.go
+++ cmd/go-cache-plugin/setup.go
@@ -28,11 +28,11 @@ import (
 	"github.com/creachadair/mhttp/proxyconn"
 	"github.com/creachadair/taskgroup"
 	"github.com/creachadair/tlsutil"
+	"github.com/doitforme/go-cache-plugin/lib/gobuild"
+	"github.com/doitforme/go-cache-plugin/lib/modproxy"
+	"github.com/doitforme/go-cache-plugin/lib/revproxy"
+	"github.com/doitforme/go-cache-plugin/lib/s3util"
 	"github.com/goproxy/goproxy"
-	"github.com/tailscale/go-cache-plugin/lib/gobuild"
-	"github.com/tailscale/go-cache-plugin/lib/modproxy"
-	"github.com/tailscale/go-cache-plugin/lib/revproxy"
-	"github.com/tailscale/go-cache-plugin/lib/s3util"
 	"tailscale.com/tsweb"
 )
 
--- lib/modproxy/modproxy.go
+++ lib/modproxy/modproxy.go
@@ -23,8 +23,8 @@ import (
 
 	"github.com/creachadair/atomicfile"
 	"github.com/creachadair/taskgroup"
+	"github.com/doitforme/go-cache-plugin/lib/s3util"
 	"github.com/goproxy/goproxy"
-	"github.com/tailscale/go-cache-plugin/lib/s3util"
 	"golang.org/x/sync/semaphore"
 )
 
